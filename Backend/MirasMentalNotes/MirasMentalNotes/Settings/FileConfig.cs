db18bfd8179270a1fdc5fd7a8902009ff8272664
Bunny <evolvingbunny@gmail.com>
2022-08-19 21:22:45 +0200

    wip: add config handling for content directory

---
+++ Backend/MirasMentalNotes/MirasMentalNotes/Settings/FileConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace MirasMentalNotes.Settings
+{
+    public class FileConfig
+    {
+        public string? ContentDirectory { get; set; }
+
+        public static FileConfig? LoadFromFile() 
+        {
+            if (!File.Exists(Settings.FileConfigPath)) return null;
+
+            var configString = File.ReadAllText(Settings.FileConfigPath);
+            return JsonSerializer.Deserialize<FileConfig>(configString);
+        }
+
+        public void SaveToFile()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var configString = JsonSerializer.Serialize(this, options);
+
+            File.WriteAllText(Settings.FileConfigPath, configString);
+        }
+    }
+}
---
+++ Backend/MirasMentalNotes/MirasMentalNotes/Settings/Settings.cs
@@ -0,0 +1,13 @@
+﻿namespace MirasMentalNotes.Settings
+{
+    public static class Settings
+    {
+        public const string FileConfigPath = "Settings/fileConfig.json";
+        public static FileConfig FileConfig { get; set; } = new FileConfig();
+
+        public static void Initialize()
+        {
+            Settings.FileConfig = FileConfig.LoadFromFile();
+        }
+    }
+}
